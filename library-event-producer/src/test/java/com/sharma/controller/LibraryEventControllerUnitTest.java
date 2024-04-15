8688d98a12fe26f7fd3e6ba0cfe9fb5ad854bdde
rajesh <rajesh9616@gmail.com>
2024-04-15 23:39:03 +0530

    Added Unit Testcase for Update Event

--- library-event-producer/src/test/java/com/sharma/controller/LibraryEventControllerUnitTest.java
+++ library-event-producer/src/test/java/com/sharma/controller/LibraryEventControllerUnitTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sharma.TestUtil;
 import com.sharma.domain.LibraryEvent;
 import com.sharma.producer.LibraryEventsProducer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -66,5 +67,50 @@ public class LibraryEventControllerUnitTest {
 
         // then
 
+    }
+
+
+    @Test
+    void updateLibraryEvent() throws Exception {
+        // given
+        var libraryEvent = objectMapper.writeValueAsString(TestUtil.libraryEventRecordUpdate());
+        when(libraryEventsProducer.sendLibraryEventSynchronous(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/libraryevent")
+                        .content(libraryEvent)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("Update Library event when event-id is null")
+    @Test
+    void updateLibraryEvent_EventId_null() throws Exception {
+        // given
+        var libraryEvent = objectMapper.writeValueAsString(TestUtil.libraryEventRecordUpdateWithNullLibraryEventId());
+        when(libraryEventsProducer.sendLibraryEventSynchronous(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/libraryevent")
+                        .content(libraryEvent)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().string("Please provide valid Library Event Id"));
+    }
+
+    @DisplayName("Update Library event when event-type is NEW")
+    @Test
+    void updateLibraryEvent_EventType_New() throws Exception {
+        // given
+        var libraryEvent = objectMapper.writeValueAsString(TestUtil.newLibraryEventRecordWithLibraryEventId());
+        when(libraryEventsProducer.sendLibraryEventSynchronous(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/libraryevent")
+                        .content(libraryEvent)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().string("Only Update Event is supported"));
     }
 }
