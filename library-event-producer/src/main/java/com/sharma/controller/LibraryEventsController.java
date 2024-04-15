6de3bfd4868c8fc5c46f814209657d9263471c59
rajesh <rajesh9616@gmail.com>
2024-04-15 23:21:19 +0530

    Added Update Library Event and Kafka Commands

--- library-event-producer/src/main/java/com/sharma/controller/LibraryEventsController.java
+++ library-event-producer/src/main/java/com/sharma/controller/LibraryEventsController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,6 @@ public class LibraryEventsController {
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("libraryEvent : {} ", libraryEvent);
 
-
         /*if (LibraryEventType.NEW != libraryEvent.libraryEventType()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only NEW event type is supported");
         }*/
@@ -38,5 +38,31 @@ public class LibraryEventsController {
         //libraryEventProducer.sendLibraryEvent(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+        log.info("libraryEvent : {} ", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+        //invoke the kafka producer
+        // libraryEventsProducer.sendLibraryEvent(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_Approach2(libraryEvent);
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please provide valid Library Event Id");
+        }
+
+        if ( ! libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE) ) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only Update Event is supported");
+
+        }
+        return null;
     }
 }
